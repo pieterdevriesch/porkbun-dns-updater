402b568e5c8476ccd64fa6d49a851a5e7fa14a48
Piet De Vriesch <34084026+pieterdevriesch@users.noreply.github.com>
2024-08-01 15:28:04 +0200

    Update status.sh
    Modified the script to remove dependencies. Now only using standard shell commands that should work on almost any system.

--- porkbun/status.sh
+++ porkbun/status.sh
@@ -1,35 +1,68 @@
 #!/bin/sh
+
+# load secrets so they don't need to be hardcoded in the script. Replace with something more secure if needed.
 source ./.credentials
+
+# enable logging of all output streams. Use logrotate to manage your logs, by default will just always append.
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>./status.log 2>&1
 
+# make sure required tools are installed. I used wget since it's the most default option.
+command -v wget >/dev/null 2>&1 || { echo >&2 "I require wget but it's not installed.  Aborting."; exit 1; }
+
+# echo the current time and date for the log.
 now=$(date)
 echo "Script executing at $now"
 
+# check for the presense of status.json, will not exist during the first run, so will create it.
+if [ ! -f ./status.json ]
+then
+    echo "status.json not found. Retrieving Porkbun DNS record now."
+    status=$(wget --header="Content-type: application/json" \
+  --post-data='{"secretapikey":"'$secretapikey'","apikey":"'$apikey'"}' \
+  --no-check-certificate \
+  --output-document - \
+  --quiet \
+  https://porkbun.com/api/json/v3/dns/retrieveByNameType/$domain'/A/*')
+    # save output to file so we don't have to connect to porkbun every 5 minutes but only if the ip has changed.
+    echo "$status" > ./status.json
+fi
+
 # extract IP address. if first run will give error and then fail the compare, so will create file after.
-dns_ip=$(jq -r '.records[].content' ./status.json)
+dns_ip=$(grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' ./status.json)
 
 # display ip for debug
 echo "DNS points to IP $dns_ip"
 
 # check current WAN IP:
-wan_ip=$(curl -s -k http://canhazip.com)
+wan_ip=$(wget --no-check-certificate --output-document - --quiet http://canhazip.com)
 
 #display output for debug purposes
 echo "Current WAN IP is $wan_ip"
 
 # compare both IP's and change porkbun setting if not the same.
-if [ $dns_ip != $wan_ip ]; then
+if [ "$dns_ip" != "$wan_ip" ]; then
   echo "Porkbun DNS setting is not correct! Changing now!"
   # update DNS of wildcard record via API IF it has changed since the last time.
-  change=$(curl -s -k --header "Content-type: application/json" --request POST --data '{"secretapikey":"'$secretapikey'","apikey":"'$apikey'","content":"'$wanip'"}' https://porkbun.com/api/json/v3/dns/editByNameType/$domain/A/*)
+  change=$(wget --header="Content-type: application/json" \
+  --post-data='{"secretapikey":"'$secretapikey'","apikey":"'$apikey'","content":"'$wanip'"}' \
+  --no-check-certificate \
+  --output-document - \
+  --quiet \
+  https://porkbun.com/api/json/v3/dns/editByNameType/$domain'/A/*')
+
   sleep 10
+
   # now retrieve current IP set in porkbun dns
-  status=$(curl -s -k --header "Content-type: application/json" --request POST --data '{"secretapikey":"'$secretapikey'","apikey":"'$apikey'"}' https://porkbun.com/api/json/v3/dns/retrieveByNameType/$domain/A/*)
+  status=$(wget --header="Content-type: application/json" \
+  --post-data='{"secretapikey":"'$secretapikey'","apikey":"'$apikey'"}' \
+  --no-check-certificate \
+  --output-document - \
+  --quiet \
+  https://porkbun.com/api/json/v3/dns/retrieveByNameType/$domain'/A/*')
   # save output to file so we don't have to connect to porkbun every 5 minutes but only if the ip has changed.
   echo "$status" > ./status.json
 else
   echo "WAN IP matches DNS record. All good."
 fi
-
